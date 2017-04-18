1a47e0c993730137610386d3947473fddbb7ef4f
shrhe_tsau <boy20212@gmail.com>
2017-04-18 13:23:33 +0800

    using scope to write order by created_at to group's post

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
 	belongs_to :user
 	belongs_to :group
 	validates :content, :presence => { :message => "文章內容不可為空白"}
+	scope :recent, -> { order("created_at DESC")}
 end

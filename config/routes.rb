6007512093d77fdaa8703195ac7f2d1ec0965e7d
shrhe_tsau <boy20212@gmail.com>
2017-04-18 11:11:14 +0800

    add posts nested route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :groups
+  resources :groups do
+  	resources :posts
+  end
   root 'groups#index'
   # 指向 views\welcome資料夾裡的index.html.erb
   # root 'welcome#index'

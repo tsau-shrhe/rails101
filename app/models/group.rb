967dfb4bd1fd69e19802c807417508db231f7e9d
shrhe_tsau <boy20212@gmail.com>
2017-04-18 13:58:15 +0800

    implement group relation

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,8 @@ class Group < ApplicationRecord
 	belongs_to :user
 	has_many :posts
 	validates :title, :presence => { :message => "標題欄位不可為空白"}
+
+	#設定group與user的關係
+	has_many :group_relationships
+	has_many :members, through: :group_relationships, source: :user
 end
